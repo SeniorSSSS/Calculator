dde512607536259fbe1671748bb37338a9b16c02
SeniorSSSS <sakariye.sed@outlook.com>
2022-06-04 12:42:41 -0400

    Add function to do multiple digit calculations

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const calculatorscreen = document.querySelector("calculatorscreen");
 const displayscreennode = document.createElement('div');
 displayscreennode.id = "displayscreen";
 calculatorscreen.appendChild(displayscreennode);
-displayscreennode.innerText = "holdertexttilcalcsmade";
+displayscreennode.innerText = "Enter Some #\'s";
 
-// LOok for buttons in html so i can work on them
+// Look for buttons in html so i can work on them
 const buttonnode = document.querySelectorAll('button');
 const buttonnodearray = Array.from(buttonnode);
 for (var i = 0; i < buttonnodearray.length; i++) {
@@ -16,54 +16,53 @@ for (var i = 0; i < buttonnodearray.length; i++) {
 
 // State variables
 var calcNumber = {
-    firstNumber: '',
-    secondNumber: '',
+    firstNumber: [],
+    secondNumber: [],
 }
 var operation = '';
 var pastCalculationNumber;
+var firstMathNumber;
+var secondMathNumber;
 
 
 function mainCalculator(event) {
     if (["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"].includes(event.target.innerText)) {
         displayscreennode.innerText = event.target.innerText;
         if (operation != '') {
-            calcNumber['secondNumber'] = event.target.innerText;
-            displayscreennode.innerText = (calcNumber['firstNumber'] + operation +calcNumber['secondNumber'])
+            displayscreennode.innerText = (calcNumber['firstNumber'] + operation +calcNumber['secondNumber']);
+            calcNumber['secondNumber'].push(event.target.innerText);
+            secondMathNumber = calcNumber['secondNumber'].join('');
+            displayscreennode.innerText = (firstMathNumber + operation +secondMathNumber);
         }
         else {
-            calcNumber['firstNumber'] = event.target.innerText;
-            displayscreennode.innerText = (calcNumber['firstNumber'] + operation +calcNumber['secondNumber'])
+            calcNumber['firstNumber'].push(event.target.innerText);
+            firstMathNumber = calcNumber['firstNumber'].join('');
+            displayscreennode.innerText = firstMathNumber;
         }
-        console.log(event.target.innerText);
-        console.table(calcNumber);
     }
     else if (['-', "/", "+", "-", "*"].includes(event.target.innerText)) {
         operation =event.target.innerText;
         console.log(operation + 'isoperation');
-        // "'" + event.target.innerText + "'"
         return operation;
     }
-    else if (event.target.innerText = '=') {
-        firstNumber = Number(calcNumber.firstNumber);
-        secondNumber = Number(calcNumber.secondNumber);
-        console.log(operation);
-        //operation = '-';
-        console.log(firstNumber + 'fin');
-        console.log(secondNumber + 'sn');
-        console.log(operation);
-        console.log(operate(firstNumber,secondNumber, operation));
-        displayscreennode.innerText = operate(firstNumber,secondNumber, operation);
+    else if (event.target.innerText == '=') {
+        firstMathNumber = Number(firstMathNumber);
+        secondMathNumber = Number(secondMathNumber);
+        console.log(operation + 'whyy');
+        displayscreennode.innerText = operate(firstMathNumber,secondMathNumber, operation);
     }
-    else {
+    else if (event.target.innerText = 'clear') {
         displayscreennode.innerText = event.target.innerText;
-        calcNumber['firstNumber']= '';
-        calcNumber['secondNumber'] = '';
+        calcNumber['firstNumber'].length = 0;
+        calcNumber['secondNumber'].length = 0;
         operation = '';
         console.log('clear');
-    }
-
+        displayscreennode.innerText = 'Enter Some #\'s';
+        
+    } else {
+      alert ('happytime');
 }
-
+}
 
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
